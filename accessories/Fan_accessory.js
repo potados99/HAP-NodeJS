4382922528af602523f765ae3242e1df46ab6d7e
potados99 <potados99@gmail.com>
2018-08-27 00:50:56 +0900

    .

--- accessories/Fan_accessory.js
+++ accessories/Fan_accessory.js
@@ -9,7 +9,7 @@ var uuid = require('../').uuid;
 var exec = require('child_process').exec;
 
 // here's a hardware device that we'll expose to HomeKit
-var FAN = {
+var FanController = {
   power: false,
   rSpeed: 100,
 
@@ -32,7 +32,7 @@ var FAN = {
 
   setSpeed: function(value) {
     console.log("Setting fan rSpeed to %s", value);
-    FAN.rSpeed = value;
+    this.rSpeed = value;
     //put your code here to set the fan to a specific value
   },
 
@@ -44,36 +44,36 @@ var FAN = {
 }
 
 // This is the Accessory that we'll return to HAP-NodeJS that represents our fake fan.
-var fan = exports.accessory = new Accessory('Fan', uuid.generate('hap-nodejs:accessories:Fan'));
+var fanAccessory = exports.accessory = new Accessory('Fan', uuid.generate('hap-nodejs:accessories:Fan'));
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-fan.username = "1A:2B:3C:4D:5E:FF";
-fan.pincode = "031-45-154";
+fanAccessory.username = "1A:2B:3C:4D:5E:FF";
+fanAccessory.pincode = "031-45-154";
 
 // set some basic properties (these values are arbitrary and setting them is optional)
-fan
+fanAccessory
 .getService(Service.AccessoryInformation)
 .setCharacteristic(Characteristic.Manufacturer, "Sample Company")
 
 // listen for the "identify" event for this Accessory
-fan.on('identify', function(paired, callback) {
-  FAN.identify();
+fanAccessory.on('identify', function(paired, callback) {
+  FanController.identify();
   callback(); // success
 });
 
 // Add the actual Fan Service and listen for change events from iOS.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-fan
+fanAccessory
 .addService(Service.Fan, "Fan") // services exposed to the user should have "names" like "Fake Light" for us
 .getCharacteristic(Characteristic.On)
 .on('set', function(value, callback) {
-  FAN.setPowerOn(value);
+  FanController.setPowerOn(value);
   callback(); // Our fake Fan is synchronous - this value has been successfully set
 });
 
 // We want to intercept requests for our current power state so we can query the hardware itself instead of
 // allowing HAP-NodeJS to return the cached Characteristic.value.
-fan
+fanAccessory
 .getService(Service.Fan)
 .getCharacteristic(Characteristic.On)
 .on('get', function(callback) {
@@ -84,7 +84,7 @@ fan
 
   var err = null; // in case there were any problems
 
-  if (FAN.powerOn) {
+  if (FanController.powerOn) {
     callback(err, true);
   }
   else {
@@ -93,13 +93,13 @@ fan
 });
 
 // also add an "optional" Characteristic for spped
-fan
+fanAccessory
 .getService(Service.Fan)
 .addCharacteristic(Characteristic.RotationSpeed)
 .on('get', function(callback) {
-  callback(null, FAN.rSpeed);
+  callback(null, FanController.rSpeed);
 })
 .on('set', function(value, callback) {
-  FAN.setSpeed(value);
+  FanController.setSpeed(value);
   callback();
 })
